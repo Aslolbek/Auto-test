8aa381f0cf45a524bccf75fe1495a1dec3a1f510
Your Name <asrorabdimannonov363@gmail.com>
2025-03-18 12:51:19 +0500

    hatolik tog'rilandi

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ const { connect } = require("mongoose")
 const db = process.env.DB
 async function connectDB() {
     try {
-        await connect(`mongodb+srv://asrorabdimannonov363:<cYPPPNL9lThRQ5PF>@mycluster.agbfq.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster`, {
+        await connect(`mongodb+srv://asrorabdimannonov363:<n2GlcS0SGzMq5WY1>@mycluster.agbfq.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
           });
